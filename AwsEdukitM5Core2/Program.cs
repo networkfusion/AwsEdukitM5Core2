d256a9b1ed41f7bca057ff96c00a41b989a7347b
Robin Jones <robin@networkfusion.co.uk>
2022-05-11 15:50:11 +0100

    Uncomment local test runner upload

--- AwsEdukitM5Core2/Program.cs
+++ AwsEdukitM5Core2/Program.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// Latest known working interpreter = `1.8.0.100`
+// Latest known working interpreter = `1.8.0.129`
 // Perform updates using:
 // nanoff --masserase --target M5Core2 --update --serialport COM16
 
