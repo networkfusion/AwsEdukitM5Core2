22f2f909aa63049417f3dd05361f30b0f4c5871f
Robin Jones <networkfusion@users.noreply.github.com>
2024-10-06 14:59:47 +0100

    Update nugets

--- AwsEdukitM5Core2/Program.cs
+++ AwsEdukitM5Core2/Program.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// Latest known working interpreter = `1.12.0.76`
+// Latest known working interpreter = `1.12.0.166`
 // Perform updates using:
 // nanoff --target M5Core2 --update --serialport COM7 --masserase
 
